c10daca93c5b621f4ccd65c6c1bb24a9430dc0ce
Khalil Merine <merinekh@gmail.com>
2023-11-25 19:00:25 -0500

    Add ressources

---
+++ components/SlideShow.jsx
@@ -0,0 +1,103 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+const Slideshow = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const slides = [
+    {
+      image: "home-caroussel-hero-1.png",
+      text: "Slide 1 Description",
+      buttonLink: "/link-to-slide1",
+    },
+    {
+      image: "home-caroussel-hero-2.png",
+      text: "Slide 2 Description",
+      buttonLink: "/link-to-slide2",
+    },
+    {
+      image: "home-caroussel-hero-3.png",
+      text: "Slide 3 Description",
+      buttonLink: "/link-to-slide3",
+    },
+    // Add more slides as needed
+  ];
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isPlaying) {
+      intervalId = setInterval(() => {
+        setCurrentSlide((prevSlide) =>
+          prevSlide === slides.length - 1 ? 0 : prevSlide + 1
+        );
+      }, 3000); // Change the interval duration as needed
+    }
+
+    return () => clearInterval(intervalId);
+  }, [currentSlide, isPlaying, slides]);
+
+  const handleDotClick = (index) => {
+    setCurrentSlide(index);
+  };
+
+  const handlePausePlay = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  const goToPrevSlide = () => {
+    setCurrentSlide((prev) => (prev === 0 ? slides.length - 1 : prev - 1));
+  };
+
+  const goToNextSlide = () => {
+    setCurrentSlide((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+  };
+
+  return (
+    <div className="slideshow">
+      <div className="slides">
+        {slides.map((slide, index) => (
+          <div
+            key={index}
+            className={`slide ${index === currentSlide ? "active" : ""}`}
+          >
+            <img src={`/images/${slide.image}`} alt={`Slide ${index + 1}`} />
+            {/* <div className="slide-text">
+              <p>{slide.text}</p>
+              <a href={slide.buttonLink}>Link</a>
+            </div> */}
+          </div>
+        ))}
+      </div>
+      <div className="controls flex align-baseline py-4">
+        <button onClick={goToPrevSlide}>{<ChevronLeftIcon />}</button>
+        <div className="dots">
+          {slides.map((_, index) => (
+            <span
+              key={index}
+              ChevronRightIcon
+              className={`dot ${index === currentSlide ? "active" : ""}`}
+              onClick={() => handleDotClick(index)}
+            />
+          ))}
+        </div>
+        <button onClick={goToNextSlide}>{<ChevronRightIcon />}</button>
+
+        <button onClick={handlePausePlay}>
+          {isPlaying ? (
+            <PauseIcon className="text-white" />
+          ) : (
+            <PlayArrowIcon className="text-white" />
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Slideshow;
