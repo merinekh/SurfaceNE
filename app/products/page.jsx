409c7e2800b6f67b58e587b5b3d05a145ff43aeb
Khalil Merine <merinekh@gmail.com>
2024-02-13 21:14:39 -0500

    Files missing

---
+++ app/products/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import "../globals.scss";
+import ProductsMainSections from "@/components/ProductsMainSections";
+
+const products = () => {
+  return (
+    <>
+      <ProductsMainSections />
+    </>
+  );
+};
+
+export default products;
