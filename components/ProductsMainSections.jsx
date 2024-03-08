7ed671a4988be550da7df159c774fe33ec6e923e
Khalil Merine <merinekh@gmail.com>
2024-03-08 01:18:11 -0500

    fix link

--- components/ProductsMainSections.jsx
+++ components/ProductsMainSections.jsx
@@ -30,7 +30,7 @@ const renderSection = (title, content, link, imgsrc) => {
       <img
         src={imgsrc}
         alt=""
-        className="w-full lg:w-1/3 lg:order-1 mb-2 lg:mb-0 sm:rounded-2xl	"
+        className="w-full lg:w-1/3 lg:order-1 mb-2 lg:mb-0 sm:rounded-2xl "
       />
       <div className="max-w-3xl text-center px-2 sm:px-3 lg:text-left lg:order-0">
         <div className="text-white">
