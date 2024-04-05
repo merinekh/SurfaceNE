fbda53aa532d06d881ca3709c6542555bced3d76
Khalil Merine <merinekh@gmail.com>
2024-04-05 02:30:36 +0100

    add speed tracker

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import Header from "@/components/Header";
@@ -60,6 +61,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
         <Analytics />
+        <SpeedInsights />
         <CookieBanner />
       </body>
     </html>
